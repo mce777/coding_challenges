90477f014d315d2e7dd9df86d6ae22998ca6cfbb
mce777 <mce777@gmail.com>
2015-11-11 20:51:52 +0000

    initial commit

---
+++ ib2_v1.rb
@@ -0,0 +1,80 @@
+class Image
+	attr_accessor :new_ary
+
+	def initialize(grid)
+		@grid = grid
+		# @new_ary = [] (couldn't get that to work)
+	end
+    
+  # the orig set of numbers
+	def output_image
+		@grid.each do |i|
+			i.each do |x|
+				print "#{x} "
+			end
+		puts
+		end	
+	end
+	
+	# image blur method
+	def blur_image2
+		@grid.each_with_index do |row, row_index|
+			row.each_with_index do |pixel, col_index|
+				if pixel == 1
+					# checking if column to RIGHT exists
+					if col_index+1 < row.length
+				    	@grid[row_index][col_index+1] = 7
+				    	
+				    	# @new_ary << pixel (couldn't get that do work)
+				    end
+				    # checking if column to LEFT exists
+				    if col_index-1 >= 0
+				    	@grid[row_index][col_index-1] = 7
+				    end
+				    # checking if row BELOW exists
+				    if row_index+1 < @grid.length
+				    	@grid[row_index+1][col_index] = 7
+				    end
+				    # checking if row ABOVE exists
+				    if row_index-1 >= 0
+				    	@grid[row_index-1][col_index] = 7
+				    end
+				end
+			end
+
+		end	
+	end
+
+	def new_blur
+		@grid.each do |i|
+			i.each do |x|
+				if x == 0
+					print "#{x} "
+				end
+				if x == 1
+					print "#{x} "
+				end
+				if x == 7
+					print "#{1} "
+				end	
+			end
+		puts	
+		end	
+	end
+
+
+end
+	
+
+image = Image.new([
+  [0, 0, 0, 0],
+  [0, 1, 0, 0],
+  [0, 0, 0, 1],
+  [1, 0, 1, 0]
+])
+
+image.output_image
+puts "- - - -"
+image.blur_image2
+image.new_blur
+
