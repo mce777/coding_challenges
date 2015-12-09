1ad3fcdcfcfc299e749c2d13d5e8f2b3cdb409da
Mark Eberhard <mce777@gmail.com>
2015-12-09 10:59:05 +0000

    recursive collatz working

--- collatz2.rb
+++ collatz2.rb
@@ -12,25 +12,23 @@ def collatz(num)
 
 end
 
-puts collatz(6)
+def longest(bottom,top)
+  #arr = []
+  name = 0
+  height = 0
 
-# def longest(bottom,top)
-#   #arr = []
-#   name = 0
-#   height = 0
-#
-#   (bottom..top).each do |new_person|
-#
-#     new_height = collatz(new_person)
-#     if new_height > height
-#       height = new_height
-#       name = new_person
-#     end
-#
-#   end
-#
-#   return [name, height]
-# end
-#
-# result = longest(1,1000000)
-# puts "#{result[0]} -- #{result[1]}"
+  (bottom..top).each do |new_person|
+
+    new_height = collatz(new_person)
+    if new_height > height
+      height = new_height
+      name = new_person
+    end
+
+  end
+
+  return [name, height]
+end
+
+result = longest(1,1000000)
+puts "#{result[0]} -- #{result[1]}"
